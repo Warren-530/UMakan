76d607232189e0d5fea6238e90015ff03915666a
LIMJIALIH <24004471@siswa.um.edu.my>
2024-11-27 14:50:59 +0800

    First Try-Warren

---
+++ UMEat/src/main/java/com/mycompany/umeat/UMEat.java
@@ -0,0 +1,13 @@
+package com.mycompany.umeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class UMEat {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UMEat.class,args);
+    }
+}
