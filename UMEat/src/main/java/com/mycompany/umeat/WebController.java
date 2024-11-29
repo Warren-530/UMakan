7d7ab225bae5ca04d7943a852fcf5be218909a47
Warren-530 <warrensaw530@gmail.com>
2024-11-29 12:55:30 +0800

    Add some pages punya html and css

--- UMEat/src/main/java/com/mycompany/umeat/WebController.java
+++ UMEat/src/main/java/com/mycompany/umeat/WebController.java
@@ -5,9 +5,44 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebController {
-    
+
     @GetMapping("/")
-    public String home(){
-        return "index";
+    public String home() {
+        return "homepage";
+    }
+
+    @GetMapping("/he-she")
+    public String heShe() {
+        return "he-she";
+    }
+
+    @GetMapping("/driverRegistration")
+    public String driverRegistration() {
+        return "driverRegistration";
+    }
+
+    @GetMapping("/kk12")
+    public String kk12() {
+        return "kk12";
+    }
+
+    @GetMapping("/t-c")
+    public String TAndC() {
+        return "TAndC";
+    }
+
+    @GetMapping("/paymenthalfportion")
+    public String paymenthalfportion() {
+        return "paymentHalfPortion";
+    }
+
+    @GetMapping("/paymentfullportion")
+    public String paymentfullportion() {
+        return "paymentFullPortion";
+    }
+
+    @GetMapping("/ordercomfirm")
+    public String ordercomfirm() {
+        return "orderComfirm";
     }
 }
